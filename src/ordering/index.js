be5b69cb5cc99e3173d59a250597835ce6f1585e
Eric Musyoka <musyokaeric@live.com>
2026-02-11 18:30:40 +0300

    Develop Ordering Microservice for Consuming Checkout Events from AWS EventBridge with AWS S

--- src/ordering/index.js
+++ src/ordering/index.js
@@ -1,9 +1,49 @@
+import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+import dynamoDbClient from "./dynamoDbClient";
+import { marshall } from "@aws-sdk/util-dynamodb";
+
 exports.handler = async function (event) {
     console.log("Received event:", JSON.stringify(event, null, 2));
-    // Process the event and perform necessary actions
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "text/plain" },
-        body: `Hello from Ordering! You've hit ${event.path}\n`,
-    };
+    // TODO - Catch and process async EventBridge Invocation and Sync API Gateway Invocation
+
+    const eventType = event['detail-type'];
+    if (eventType) {
+        await eventBridgeInvocation(event);
+    } else {
+        await apiGatewayInvocation(event);
+    }
+}
+
+const eventBridgeInvocation = async (event) => {
+    console.log("Processing EventBridge Invocation");
+    // TODO - Process EventBridge Invocation
+    await createOrder(event.detail);
+}
+
+const apiGatewayInvocation = async (event) => {
+    console.log("Processing API Gateway Invocation");
+    // TODO - Process API Gateway Invocation
+}
+
+const createOrder = async (basketCheckoutEvent) => {
+    console.log("Creating order:", JSON.stringify(basketCheckoutEvent, null, 2));
+    // TODO - Create order in DynamoDB
+
+    try {
+        const orderDate = new Date().toISOString();
+        basketCheckoutEvent.orderDate = orderDate;
+        console.log(basketCheckoutEvent)
+
+        const params = {
+            TableName: process.env.DYNAMO_TABLE_NAME,
+            Item: marshall(basketCheckoutEvent || {})
+        };
+
+        const result = await dynamoDbClient.send(new PutItemCommand(params));
+        console.log("Order created successfully:", result);
+        return result;
+    } catch (error) {
+        console.error("Error creating order:", error);
+        throw error;
+    }
 }
